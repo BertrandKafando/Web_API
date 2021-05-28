5fda229b780517237da34fe16b34dbab8598f6f2
Bertrand KAFANDO <bertrandkafando07@gmail.com>
2021-05-29 00:04:59 +0100

    test-cdn

--- kafando/public/javascripts/javajs.js
+++ kafando/public/javascripts/javajs.js
@@ -1,20 +1,14 @@
-//const { Json } = require("sequelize/types/lib/utils");
-
-//const { json } = require("express");
- //function getuse(){ 
-let users=[];
-const xhr= new XMLHttpRequest()
-xhr.open('Get','/users')
-xhr.setRequestHeader('conten-type','application/json')
+function getusers(cb){
+   let xhr=new XMLHttpRequest()
+   xhr.open('GET','/users')
+   xhr.setRequestHeader('contente-type','application/json')
 
 
-xhr.onload()=() =>{
-   // if(xhr.status===200){
-        console.log(JSON.parse(xhr.responseText))
-        //console.log(users);
-   // }
-}
-
- //}
-
-xhr.send()
+   xhr.onload=()=>{
+   if(xhr.status==200){
+       console.log(xhr.response)
+       cb(JSON.parse(xhr.responseText))
+   }
+   }
+   xhr.send()
+   }
