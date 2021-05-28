700ad57072b56d75368f4293738110a003ee109b
Bertrand KAFANDO <bertrandkafando07@gmail.com>
2021-05-28 21:33:29 +0100

    usersok-16

--- kafando/routes/users.js
+++ kafando/routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
+
 var router = express.Router();
 
+const usersRepo = require('../repositories/users')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async function(req, res, next) {
+
+res.send(await usersRepo.getUsers( req.query.offset, req.query.limit))
+
+});
+
+router.post('/',async function(req, res, next) {
+
+res.send( await usersRepo.addUser( req.body.user ) );
+
+});
+
+router.put('/',async function(req, res, next) {
+
+res.send( await usersRepo.updateUser( req.body.user ) );
+
+});
+
+router.get('/:id',async function(req, res, next) {
+
+res.send( await usersRepo.getUser( req.params.id) );
+
+});
+
+router.delete('/:id',async function(req, res, next) {
+
+res.send( await usersRepo.deleteUser( req.bo ) );
+
 });
 
 module.exports = router;
